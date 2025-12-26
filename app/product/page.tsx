5ba234480942f111d2a54287697009d4ea76d886
khanshoaibraza004 <khanshoaibraza004@gmail.com>
2025-12-26 19:01:00 +0530

    fix error

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -5,9 +5,13 @@ import FaqSection from "@/components/faqSection/FaqSection";
 import ReadyToOrder from "@/components/readyToOrder/ReadytoOrder";
 import PartnerWithUs from "@/components/partnerwithus/PartnerWith";
 import RequirementModal from "@/components/modal/TellUsModal";
-import PriceList from "./ProductListing";
 import { useTheme } from "@/hooks/use-theme";
 import PageAdvertising from "@/components/advertising/pageAdvetise";
+import dynamic from "next/dynamic";
+
+
+
+const PriceList = dynamic(() => import("./ProductListing"), { ssr: false });
 
 export default function Product() {
   const { theme } = useTheme();
@@ -15,7 +19,6 @@ export default function Product() {
   return (
     <div>
       <PageAdvertising />
-      {/* <ProductList /> */}
       <div className="bg-white">
         <PriceList />
       </div>
